7c99181a1ac8372b930ac6cd82fb7ef93c03fe25
s28094 <s28094@pjwstk.edu.pl>
2024-04-23 19:56:47 +0200

    Created Animal.cs

---
+++ Animal.cs
@@ -0,0 +1,10 @@
+namespace APBD_05;
+
+public class Animal
+{
+    public int IdAnimal { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public string Area { get; set; }
+}
